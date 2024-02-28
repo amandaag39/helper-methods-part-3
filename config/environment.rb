1f369c8ce3d747dab6a7e80390433a855d973d39
Amanda Mendez <amandaag39@gmail.com>
2024-02-28 10:23:52 -0600

    users added with devise and authentication added, all tests pass

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Devise defined default url
+# config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
